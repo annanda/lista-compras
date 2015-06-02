e90b71efc38c87436308fb9e6dddab852b035676
Annanda Sousa <annanda.sousa@gmail.com>
2015-06-02 16:49:26 -0300

    Adicionando o modelo no Admin

--- lista_compra/models.py
+++ lista_compra/models.py
@@ -5,3 +5,6 @@ from django.utils import timezone
 class Item(models.Model):
     nome = models.CharField(max_length = 300)
     categoria = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.nome
